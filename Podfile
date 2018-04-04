48f561ef0c84d760b3c3ff14d2c73ad206855812
Radaev Mikhail <msfrms@gmail.com>
2018-04-04 20:22:44 +0500

    Initial commit

---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+source 'https://github.com/msfrms/Spec.git'
+
+target 'ReduxSwift' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ReduxSwift
+  pod 'SUtils', '~> 0.1'
+
+  target 'ReduxSwiftTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ Source/Store.swift
@@ -0,0 +1,66 @@
+//
+// Created by Radaev Mikhail on 17.02.17.
+// Copyright (c) 2017 msfrms. All rights reserved.
+//
+
+import Foundation
+import SUtils
+
+public typealias Reducer<State> = (State, Action) -> State
+
+public protocol Action {}
+
+public protocol Subscriber {
+    associatedtype State
+    func subscribe(command: CommandWith<State>) -> Command
+}
+
+public protocol Dispatcher {
+    func dispatch(action: Action)
+}
+
+extension CommandWith: Hashable {
+
+    public var hashValue: Int { return ObjectIdentifier(self).hashValue }
+
+    public static func ==(lhs: CommandWith<T>, rhs: CommandWith<T>) -> Bool {
+        return ObjectIdentifier(lhs) == ObjectIdentifier(rhs)
+    }
+}
+
+public final class Store<State>: Dispatcher, Subscriber {
+
+    private(set) var state: State
+    private let reducer: Reducer<State>
+    private let queue: DispatchQueue
+    private var subscribers: Set<CommandWith<State>> = []
+
+    public init(queue: DispatchQueue, initialState: State, reducer: @escaping Reducer<State>) {
+        self.reducer = reducer
+        self.state = initialState
+        self.queue = queue
+    }
+
+    public convenience init(initialState: State, reducer: @escaping Reducer<State>) {
+        self.init(queue: DispatchQueue(label: "private store queue"), initialState: initialState, reducer: reducer)
+    }
+
+    @discardableResult
+    public func subscribe(command: CommandWith<State>) -> Command {
+        self.queue.async {
+            self.subscribers.insert(command)
+            command.execute(value: self.state)
+        }
+        return Command(id: "stop observing for \(command)") { [weak command] in
+            command.foreach { self.subscribers.remove($0) }
+        }
+    }
+
+    public func dispatch(action: Action) {
+        self.queue.async {
+            self.state = self.reducer(self.state, action)
+            self.subscribers.forEach { $0.execute(value: self.state) }
+        }
+    }
+}
+
