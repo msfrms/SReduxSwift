7c1dffa5f8bb0ee9ce45678d87818bcbd9764c5d
Radaev Mikhail <msfrms@gmail.com>
2018-05-17 20:38:36 +0300

    Добавил фильтрацию при подписке на изменения в сторе, для того чтобы получать изменения только для нужных Action

--- Source/Store.swift
+++ Source/Store.swift
@@ -10,30 +10,41 @@ public typealias Reducer<State> = (State, Action) -> State
 
 public protocol Action {}
 
-public protocol Subscriber {
-    associatedtype State
-    func subscribe(command: CommandWith<State>) -> Command
-}
-
 public protocol Dispatcher {
     func dispatch(action: Action)
 }
 
-extension CommandWith: Hashable {
-
-    public var hashValue: Int { return ObjectIdentifier(self).hashValue }
-
-    public static func ==(lhs: CommandWith<T>, rhs: CommandWith<T>) -> Bool {
-        return ObjectIdentifier(lhs) == ObjectIdentifier(rhs)
-    }
+public protocol Filter {
+    func filter(action: Action) -> Bool
 }
 
-public final class Store<State>: Dispatcher, Subscriber {
+public final class Store<State>: Dispatcher {
+
+    class Pass: Filter {
+        func filter(action: Action) -> Bool { return true }
+    }
+
+    class Subscriber: Hashable {
+
+        let command: CommandWith<State>
+        let filter: Filter
+
+        init(command: CommandWith<State>, filter: Filter) {
+            self.command = command
+            self.filter = filter
+        }
+
+        var hashValue: Int { return ObjectIdentifier(self).hashValue }
+
+       static func == (lhs: Subscriber, rhs: Subscriber) -> Bool {
+           return ObjectIdentifier(lhs) == ObjectIdentifier(rhs)
+        }
+    }
 
     public private(set) var state: State
     private let reducer: Reducer<State>
     private let queue: DispatchQueue
-    private var subscribers: Set<CommandWith<State>> = []
+    private var subscribers: Set<Subscriber> = []
 
     public init(queue: DispatchQueue, initialState: State, reducer: @escaping Reducer<State>) {
         self.reducer = reducer
@@ -47,19 +58,27 @@ public final class Store<State>: Dispatcher, Subscriber {
 
     @discardableResult
     public func subscribe(command: CommandWith<State>) -> Command {
+        return subscribe(command: command, for: Pass())
+    }
+
+    @discardableResult
+    public func subscribe(command: CommandWith<State>, for filter: Filter) -> Command {
+        let subscriber = Subscriber(command: command, filter: filter)
         self.queue.async {
-            self.subscribers.insert(command)
+            self.subscribers.insert(subscriber)
             command.execute(value: self.state)
         }
-        return Command(id: "stop observing for \(command)") { [weak command] in
-            command.foreach { self.subscribers.remove($0) }
-        }
+        return Command(id: "stop observing for \(subscriber)") { [weak subscriber] in
+            subscriber.foreach { self.subscribers.remove($0) }
+        }.observe(queue: self.queue)
     }
 
     public func dispatch(action: Action) {
         self.queue.async {
             self.state = self.reducer(self.state, action)
-            self.subscribers.forEach { $0.execute(value: self.state) }
+            self.subscribers
+                .filter { $0.filter.filter(action: action) }
+                .forEach { $0.command.execute(value: self.state) }
         }
     }
 }
