0d5929ac0b0a8094cc077760fa4408a393f02528
Radaev Mikhail <msfrms@gmail.com>
2019-12-27 19:00:47 +0500

    add middleware

--- Source/Store.swift
+++ Source/Store.swift
@@ -6,79 +6,76 @@
 import Foundation
 import SUtils
 
-public typealias Reducer<State> = (State, Action) -> State
-public typealias SubscriberFilter<State> = (State, State, Action) -> Bool
-
 public protocol Action {}
 
 public protocol Dispatcher {
     func dispatch(action: Action)
 }
 
-public struct Pass {
-    public static func always<State>(_ oldState: State, _ newState: State, _ action: Action) -> Bool { return true }
-    public static func stateHasChanged<State: Equatable>(_ oldState: State, _ newState: State, _ action: Action) -> Bool { return oldState != newState }
+public typealias Reducer<State> = (State, Action) -> State
+
+public typealias Middleware<State> = (State, Action, Dispatcher) -> Void
+
+extension CommandWith: Hashable {
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(self)
+    }
+
+    public static func == (lhs: CommandWith<T>, rhs: CommandWith<T>) -> Bool {
+        return ObjectIdentifier(lhs) == ObjectIdentifier(rhs)
+    }
 }
 
 public final class Store<State>: Dispatcher {
 
-    private class Subscriber: Hashable {
-
-        let command: CommandWith<State>
-        let pass: SubscriberFilter<State>
-
-        init(command: CommandWith<State>, filter: @escaping SubscriberFilter<State>) {
-            self.command = command
-            self.pass = filter
-        }
-
-        var hashValue: Int { return ObjectIdentifier(self).hashValue }
-
-       static func == (lhs: Subscriber, rhs: Subscriber) -> Bool {
-           return ObjectIdentifier(lhs) == ObjectIdentifier(rhs)
-        }
-    }
-
     public private(set) var state: State
     private let reducer: Reducer<State>
     private let queue: DispatchQueue
-    private var subscribers: Set<Subscriber> = []
+    private var commands: Set<CommandWith<State>> = []
+    private let middleware: Middleware<State>
 
-    public init(queue: DispatchQueue, initialState: State, reducer: @escaping Reducer<State>) {
+    public init(queue: DispatchQueue,
+                initialState: State,
+                reducer: @escaping Reducer<State>,
+                middleware: @escaping Middleware<State> = { _, _, _ in }) {
         self.reducer = reducer
         self.state = initialState
         self.queue = queue
+        self.middleware = middleware
     }
 
-    public convenience init(initialState: State, reducer: @escaping Reducer<State>) {
-        self.init(queue: DispatchQueue(label: "private store queue"), initialState: initialState, reducer: reducer)
+    public convenience init(initialState: State,
+                            reducer: @escaping Reducer<State>,
+                            middleware: @escaping Middleware<State> = { _, _, _ in }) {
+        self.init(queue: DispatchQueue(label: "private store queue"),
+                  initialState: initialState,
+                  reducer: reducer,
+                  middleware: middleware)
     }
 
     @discardableResult
     public func subscribe(command: CommandWith<State>) -> Command {
-        return subscribe(command: command, with: Pass.always)
-    }
-
-    @discardableResult
-    public func subscribe(command: CommandWith<State>, with filter: @escaping SubscriberFilter<State>) -> Command {
-        let subscriber = Subscriber(command: command, filter: filter)
-        self.queue.async {
-            self.subscribers.insert(subscriber)
+        queue.async {
+            self.commands.insert(command)
             command.execute(value: self.state)
         }
-        return Command(id: "stop observing for \(subscriber)") { [weak subscriber] in
-            subscriber.foreach { self.subscribers.remove($0) }
+        return Command(id: "stop observing for \(command)") {
+            self.commands.remove(command)
         }.observe(queue: self.queue)
     }
 
+    public func setState(_ state: State) {
+        self.state = state
+        commands.forEach { $0.execute(value: state) }
+    }
+
     public func dispatch(action: Action) {
-        self.queue.async {
-            let oldState = self.state
-            let newState = self.reducer(oldState, action)
+        queue.async {
+            let newState = self.reducer(self.state, action)
             self.state = newState
-            self.subscribers
-                    .filter { $0.pass(oldState, newState, action) }
-                    .forEach { $0.command.execute(value: newState) }
+            self.commands.forEach { $0.execute(value: newState) }
+            self.middleware(newState, action, self)
         }
     }
 }
